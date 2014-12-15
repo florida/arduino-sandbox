e415f56e28150b6529f29430612bfc3b7cb87b73
Florida Elago <florida.elago@socialchorus.com>
2014-12-14 16:54:48 -0800

    servo

---
+++ servo.js
@@ -0,0 +1,16 @@
+var five = require("johnny-five"),
+    RandomColor = require('randomcolor'),
+    // or "./lib/johnny-five" when running from the source
+    board = new five.Board();
+
+board.on("ready", function() {
+  var servo = new five.Servo({
+    pin: 12,
+    type: "continuous"
+  });
+  setInterval(function(){
+    servo.ccw(1);
+  }, 500);
+});
+
+
