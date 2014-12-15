07a67d29100fe489a1e8f2eacf414c71ecdb3bee
Florida Elago <florida.elago@socialchorus.com>
2014-12-14 16:54:08 -0800

    generate random color for led

--- strobe.js
+++ strobe.js
@@ -1,20 +1,16 @@
 var five = require("johnny-five"),
+    RandomColor = require('randomcolor'),
     // or "./lib/johnny-five" when running from the source
     board = new five.Board();
 
 board.on("ready", function() {
 
-  // Create an Led on pin 13 and strobe it on/off
-  // Optionally set the speed; defaults to 100ms
-
   var led = new five.Led.RGB([ 9, 10, 11 ]);
-  //(new five.Led(13)).strobe();
-  //for(var i = 0; i >= 50; i++) {
 
-    led.color("#ff00ff");
+  setInterval(function() {
+    led.off();
+    console.log("Blinking and things");
+    led.color(RandomColor.randomColor());
     led.on();
-  //}
-
-
+  }, 1000);
 });
-
